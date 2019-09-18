bd1b64f1d229d945f8b1cc403963e928cd8276a5
elosine <studiotttguttt>
2019-09-18 15:31:08 -0400

    up

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ Object.assign=require('object-assign')
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
 
+app.use(express.static(path.join(__dirname, '/public')));
+
+ app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,

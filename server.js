4094defea6c6870b110c82e9860a37b1cdcf1239
elosine <studiotttguttt>
2019-09-18 15:37:11 -0400

    uu

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(morgan('combined'))
 app.use(express.static(path.join(__dirname, '/public')));
 
  app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, '/public/index.html'));
+  res.sendFile(path.join(__dirname, '/views/index.html'));
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
